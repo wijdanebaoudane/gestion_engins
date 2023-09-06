7eef475f7c64c7900ec7e6fd8014ce490e94fb9c
MouridZakaria <127679395+MouridZakaria@users.noreply.github.com>
2023-09-07 00:14:47 +0100

    Security Done

---
+++ Backend-Engin/src/main/java/com/example/backendengin/controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.backendengin.controllers;
+
+import com.example.backendengin.dtos.request.SignUpRequest;
+import com.example.backendengin.dtos.request.SigninRequest;
+import com.example.backendengin.dtos.response.JwtAuthenticationResponse;
+import com.example.backendengin.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
